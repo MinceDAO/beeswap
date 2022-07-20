b4992376c4693efc282dcb776e4a4d7f614eb87d
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-07-20 21:45:31 +0100

    minor update

--- iceswap-frontend/src/components/Swap.js
+++ iceswap-frontend/src/components/Swap.js
@@ -12,4 +12,16 @@ import {
     useColorModeValue,
   } from "@chakra-ui/react";
 
-  
+  export default function Swap() {
+    return (
+        <Container
+        mt={10}
+        minH={"100vh"}
+        direction={{ base: "column", md: "row" }}
+        maxW={"7x1"}
+        bg={"#1F1933"}
+        >
+
+        </Container>
+    )
+  }
